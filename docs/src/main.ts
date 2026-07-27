3d9d9cafa4d6a770713426265c32ec7f6634cca1
anthonyjclark <anthonyjclark@gmail.com>
2026-07-27 10:10:01 -0700

    Minimal working pyodide.

--- docs/src/main.ts
+++ docs/src/main.ts
@@ -1,6 +1,10 @@
 import './style.css'
+import { loadPyodide } from 'pyodide';
 
 const app = document.getElementById('app')
 if (app) {
-  app.innerHTML = '<h1>Welcome to PyMatch</h1>'
+  app.innerHTML = '<h1>App</h1>'
 }
+
+let pyodide = await loadPyodide();
+console.log(pyodide.runPython("4 + 2"));
