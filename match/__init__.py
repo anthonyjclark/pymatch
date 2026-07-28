6cfb2797cf6acf273f4252fc5347900d8a32eff6
anthonyjclark <anthonyjclark@gmail.com>
2026-07-28 09:58:24 -0700

    Adding utils and extras to top-level module.

--- match/__init__.py
+++ match/__init__.py
@@ -1,13 +1,15 @@
-from . import nn
+from . import extras, nn, utils
 from .tensor import manual_seed, ones, rand, randn, tensor, zeros
 
 __version__ = "0.1.0"
 __all__ = [
+    "extras",
     "manual_seed",
     "nn",
     "ones",
     "rand",
     "randn",
     "tensor",
+    "utils",
     "zeros",
 ]
